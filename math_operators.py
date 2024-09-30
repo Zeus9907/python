116a3710ff8755654a9b8be0f6baa8a8913927f6
= <=>
2024-09-30 20:18:43 +0530

    adding files

---
+++ math_operators.py
@@ -0,0 +1,20 @@
+# addtion operator +
+print("the sum of 3 and 4 is: ", 3 + 4)
+
+# subtraction operator -
+print("the subtraction of 3 from 4 is: ", 4 - 3)
+
+# multiplication operator *
+print("the multiplication of 3 * 4 is: ", 4 * 3)
+
+# division operator / 
+print("the division of 3 from 9 is: ", 9 / 3)
+
+# floor division // 
+print("the quotient of 2 from 9 is: ", 9 // 2)
+
+# modulus operator % 
+print("the modulus of 2 % 3 is: ", 2 % 3)
+
+# exponentiation operator **
+print("3 is exponent to base 2: ", 2 ** 3)
